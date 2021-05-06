989823c75e3cb2613caa670dffd42b8844cd78ff
Moonki Kim <kimmoonki90@lotte.net>
2021-05-06 18:23:20 +0900

    image interval

--- 01_NavigationBar1/portfolio.js
+++ 01_NavigationBar1/portfolio.js
@@ -27,3 +27,54 @@ function menuToggle() {
 }
 
 document.getElementById('toggleBtn').addEventListener('click', menuToggle);
+
+/* Welcome Area */
+
+var imageSlidIndex = 1;
+
+showImageSlide(imageSlidIndex);
+
+function imageSlideTimer() {
+    plusImageSlide(1);
+}
+
+var imageTimer = setInterval(imageSlideTimer, 5000);
+
+function plusImageSlide(n) {
+    clearInterval(imageTimer);
+    imageTimer = setInterval(imageSlideTimer, 5000);
+
+    showImageSlide(imageSlidIndex += n);
+}
+
+function currentImageSlide(n) {
+    clearInterval(imageTimer);
+    imageTimer = setInterval(imageSlideTimer, 5000);
+
+    showImageSlide(imageSlidIndex = n);
+}
+
+function showImageSlide(n) {
+    var i;
+    var slides = document.getElementsByClassName('image-slide');
+    var dots = document.getElementsByClassName('dot');
+
+    if(n > slides.length) { imageSlidIndex = 1}
+    if(n < 1) {imageSlidIndex =slides.length}
+    for( i=0; i < slides.length ; i++) { 
+        slides[i].style.display = 'none';
+    }
+    for( i=0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(' active', '');
+    }
+    slides[imageSlidIndex -1].style.display = 'block';
+    dots[imageSlidIndex -1].className += ' active';
+}
+
+document.getElementById('imagePrev').addEventListener('click', plusImageSlide.bind(null, -1));
+document.getElementById('imageNext').addEventListener('click', plusImageSlide.bind(null, 1));
+
+document.getElementById('firstDot').addEventListener('click', currentImageSlide.bind(null, 1));
+document.getElementById('secondDot').addEventListener('click', currentImageSlide.bind(null, 2));
+document.getElementById('thirdDot').addEventListener('click', currentImageSlide.bind(null, 3));
+document.getElementById('forthDot').addEventListener('click', currentImageSlide.bind(null, 4));
